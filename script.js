3d83eb7845aa7ded40e40c6f647277ba42c2f29f
loshara1423 <153355119+loshara1423@users.noreply.github.com>
2023-12-09 20:48:00 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function addReview() { 
+document.addEventListener('DOMContentLoaded', function() { 
+  loadReviews(); 
+}); 
+ 
+function addReview(event) { 
+  event.preventDefault(); 
+ 
   var name = document.getElementById('name').value; 
   var message = document.getElementById('message').value; 
  
@@ -18,7 +24,35 @@ function addReview() {
   reviewDiv.appendChild(reviewContent); 
   reviewContainer.appendChild(reviewDiv); 
  
-  // Очистка полей формы 
   document.getElementById('name').value = ''; 
   document.getElementById('message').value = ''; 
+ 
+  saveReview(name, message); 
+} 
+ 
+function saveReview(name, message) { 
+  var reviews = getReviewsFromStorage(); 
+  reviews.push({ name: name, message: message }); 
+  localStorage.setItem('reviews', JSON.stringify(reviews)); 
+} 
+ 
+function getReviewsFromStorage() { 
+  var reviews = localStorage.getItem('reviews'); 
+  return reviews ? JSON.parse(reviews) : []; 
+} 
+ 
+function loadReviews() { 
+  var reviews = getReviewsFromStorage(); 
+  var reviewContainer = document.getElementById('reviewsContainer'); 
+ 
+  reviews.forEach(function(review) { 
+    var reviewDiv = document.createElement('div'); 
+    reviewDiv.className = 'review'; 
+ 
+    var reviewContent = document.createElement('p'); 
+    reviewContent.innerHTML = '<strong>' + review.name + ':</strong> ' + review.message; 
+ 
+    reviewDiv.appendChild(reviewContent); 
+    reviewContainer.appendChild(reviewDiv); 
+  }); 
 }
